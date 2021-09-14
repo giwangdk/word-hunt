68a92e26e44102fbf5dc919839f4182946028276
gidwikintan <gidwikintan@gmail.com>
2021-09-14 17:01:35 +0700

    add useEffect hook to fetch API dictionary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Container } from '@material-ui/core';
 
 function App() {
+  const [word, setWord] = useState("")
+  const [meanings, setMeanings] = useState([])
 
   const dictionaryAPI = async () => {
     try {
       const {data} = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane")
       console.log(data);
+      setMeanings(data)
     } catch (error) {
       console.log(error);
     }
   }
 
+  console.log(meanings);
+
   useEffect(() => {
     dictionaryAPI()
-  })
+  }, [])
 
   return (
     <div className="App">
-      
+      <Container>
+        
+      </Container>
     </div>
   );
 }
