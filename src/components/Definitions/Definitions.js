966416b97ec0445c811f5e431aab811624e9ba0d
gidwikintan <gidwikintan@gmail.com>
2021-09-16 21:11:19 +0700

    showing example synonym with some condional rendering

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -12,6 +12,23 @@ const Definitions = ({word, meanings, category}) => {
                             item.definitions.map((def) => (
                                 <div className="singleMean" style={{backgroundColor:"white", color:"black"}}>
                                     <b>{def.definition}</b>
+                                    <hr style={{backgroundColor: "black", width:"100%"}}/>
+                                    {
+                                        def.example && (
+                                            <span>
+                                                <b>Example : </b>
+                                                { def.example}
+                                            </span>
+                                            )
+                                    }
+                                    {
+                                        def.synonyms && (
+                                            <span>
+                                                <b>Synonyms : </b>
+                                                { def.synonyms.map((s)=>`${s}, `)}
+                                            </span>
+                                        )
+                                    }
                                 </div>
                             ))
                         )
